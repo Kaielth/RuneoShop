02ad9bccfa535b61da35caed52d8142e98abfe55
Kaielth <ennethfultrerth@gmail.com>
2014-10-15 14:13:39 -0600

    Mision, Vision e Imagenes -3 productos-

--- app/src/main/java/ric/runeo/kaielth/rropa/MainShop.java
+++ app/src/main/java/ric/runeo/kaielth/rropa/MainShop.java
@@ -1,9 +1,12 @@
 package ric.runeo.kaielth.rropa;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 
 public class MainShop extends ActionBarActivity {
@@ -28,9 +31,30 @@ public class MainShop extends ActionBarActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
+        /*if (id == R.id.action_settings) {
             return true;
+        }*/
+        switch(id){
+            case R.id.menu_mision:
+                showAlertDialog("Misión", getString(R.string.menu_mision_description));
+                break;
+            case R.id.menu_vision:
+                showAlertDialog("Visión", getString(R.string.menu_vision_description));
+                break;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public void showAlertDialog(String title, String content){
+        new AlertDialog.Builder(this)
+                .setTitle(title)
+                .setMessage(content)
+                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                })
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .show();
     }
 }
